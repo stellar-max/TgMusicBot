cc8874af3f9f3d64d964b81c66c4ce76a9237ff9
stellar-max <serenix1611@gmail.com>
2026-03-20 00:17:47 +0530

    Update thumbnails.py

--- TgMusic/core/thumbnails.py
+++ TgMusic/core/thumbnails.py
@@ -210,7 +210,7 @@ def get_duration(duration: int, time: str = "0:24") -> str:
 
 
 async def gen_thumb(song: CachedTrack) -> str:
-    """Generates and saves a custom thumbnail for a given song.
+    """ and saves a custom thumbnail for a given song.
 
     This function orchestrates the entire thumbnail creation process:
     1. Fetches the song's cover art.
@@ -246,7 +246,7 @@ async def gen_thumb(song: CachedTrack) -> str:
     bg.paste(image, (paste_x, paste_y), image)
 
     draw = ImageDraw.Draw(bg)
-    draw.text((285, 180), "Fallen Beatz", (192, 192, 192), font=FONTS["nfont"])
+    draw.text((285, 180), "Billa Music", (192, 192, 192), font=FONTS["nfont"])
     draw.text((285, 200), title, (255, 255, 255), font=FONTS["tfont"])
     draw.text((287, 235), artist, (255, 255, 255), font=FONTS["cfont"])
     draw.text((478, 321), get_duration(duration), (192, 192, 192), font=FONTS["dfont"])
